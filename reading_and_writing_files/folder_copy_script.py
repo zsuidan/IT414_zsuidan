f5ed006769b381d7878aba60935eed9053e811bf
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2022-04-11 20:20:57 -0400

    Added comments

--- reading_and_writing_files/folder_copy_script.py
+++ reading_and_writing_files/folder_copy_script.py
@@ -3,10 +3,12 @@ from functions.filesystem_functions import *
 program_running = True
 
 while program_running:
-
+    #Calls function for copying a folder
     copyFolder()
 
+    #Prompts user asking them if they would like to copy another folder
     done_running = input("\nWould you like to copy something else? (Y/N): ")
 
+    #If N is entered, the program terminates, otherwise it continues 
     if done_running.lower() == "n":
         program_running = False
