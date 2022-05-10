d6e9650cfb944b53259047511ba5bf0be41cd460
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2022-05-09 22:17:48 -0400

    Added comments

--- documents_assignment/generate_word_doc.py
+++ documents_assignment/generate_word_doc.py
@@ -11,10 +11,9 @@ operations_file = docx.Document('text_files/operations.docx')
 
 ceo_report.add_heading("Operations", 1)
 
+    #Adds each paragraph from operations document to the ceo report
 for paragraph in operations_file.paragraphs:
     ceo_report.add_paragraph(paragraph.text)
-
-#ceo_report.add_page_break()
 
 #Adds the contents of the sales spreadsheet to the ceo report
 sales_file = openpyxl.load_workbook('text_files/sales.xlsx')
@@ -25,6 +24,7 @@ curr_sheet = sales_file.active
 max_row = curr_sheet.max_row
 max_col = curr_sheet.max_column
 
+    #Loops through the sales spreadsheet adding rows to the new table
 sales_table = ceo_report.add_table(rows=max_row, cols=max_col)
 
 row_count = 1
@@ -34,10 +34,12 @@ while row_count <= max_row:
     col_count = 1
 
     while col_count <= max_col:
+        #Formatting for margins
         if col_count == 5 and row_count > 1:
             tmp_row_cells[col_count - 1].text = str(curr_sheet.cell(row=row_count, column=col_count).value * 100) + "%"
-
+        #Formatting for other numbers
         elif col_count > 1 and row_count > 1:
+            #If the number has no commas in it already, they are added, otherwise the number is added normally
             try:
                 raw_number = curr_sheet.cell(row=row_count, column=col_count).value
                 formatted_number = "{:,}".format(raw_number)
@@ -61,14 +63,13 @@ pdfReader = PyPDF4.PdfFileReader(marketing_file)
 
 pdf_page = pdfReader.getPage(0)
 
-#Pulls text from page and fixes the extraneous spaces and new lines 
+    #Pulls text from page and fixes the extraneous spaces and new lines 
 pdf_text = pdf_page.extractText().split("\n")
 
 ceo_report.add_paragraph(pdf_text)
 
 marketing_file.close()
 
-#ceo_report.add_page_break()
 
 #Adds the contents of the IT webpage to the ceo report
 it_file = open('text_files/IT.html')
@@ -76,6 +77,7 @@ it_soup = BeautifulSoup(it_file, "lxml")
 
 ceo_report.add_heading("IT", 1)
 
+    #Pulls all paragraphs from the webpage then loops through them adding each individually
 it_paragraphs = it_soup.findAll("p")
 
 for para in it_paragraphs:
