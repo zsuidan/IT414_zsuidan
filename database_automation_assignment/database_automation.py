2d354e4b82fd15feea4a1b213f5fbd60558d5f6c
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2022-05-16 18:05:11 -0400

    Removed redundant code and added comments

--- database_automation_assignment/database_automation.py
+++ database_automation_assignment/database_automation.py
@@ -35,15 +35,7 @@ if imported_file_type.lower() == "csv":
                 my_db.executeQuery("INSERT INTO addresses_working (first_name, last_name, street, city, state, zip) VALUES ('" + csv_data[row_count][0] + "','" + csv_data[row_count][1] + "','" + csv_data[row_count][2] + "','" + csv_data[row_count][3] + "','" + csv_data[row_count][4] + "','" + csv_data[row_count][5] + "')")
                 row_count += 1
 
-            #Swaps the data of the addresses and addresses_working tables
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses TO database_automation_assignment.addresses_old")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_working TO database_automation_assignment.addresses")
-            my_db.executeQuery("COMMIT")
-
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_old TO database_automation_assignment.addresses_working")
-            my_db.executeQuery("COMMIT")
+    #Displays an error if the file is not found
     except:
         print("File not found.")
 #If the file type is json, executes the following
@@ -53,28 +45,24 @@ elif imported_file_type.lower() == "json":
             #Clears existing data in working table
             my_db.executeQuery("TRUNCATE addresses_working")
 
+            #Reads data from the json file adding it to a list
             json_data = json.load(json_file)
 
             #Fills working table with imported csv data
             for row in json_data:
                 my_db.executeQuery("INSERT INTO addresses_working (first_name, last_name, street, city, state, zip) VALUES ('" + row['first_name'] + "','" + row['last_name'] + "','" + row['street'] + "','" + row['city'] + "','" + row['state'] + "','" + row['zip'] + "')")
 
-            #Swaps the data of the addresses and addresses_working tables
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses TO database_automation_assignment.addresses_old")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_working TO database_automation_assignment.addresses")
-            my_db.executeQuery("COMMIT")
-
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_old TO database_automation_assignment.addresses_working")
-            my_db.executeQuery("COMMIT")
+    #Displays an error if the file is not found
     except:
         print("File not found.")
 #If the file type is xml, executes the following
 elif imported_file_type.lower() == "xml":
     try:
         with open("text_files/" + imported_table + ".xml") as xml_file:
+            #Removes existing data in working table
             my_db.executeQuery("TRUNCATE addresses_working")
+
+            #Reads xml data and adds values to a different list for each attribute
             file_text = xml_file.read()
             xml_data = BeautifulSoup(file_text, "xml")
 
@@ -85,19 +73,22 @@ elif imported_file_type.lower() == "xml":
             states = xml_data.find_all("state")
             zips = xml_data.find_all("zip")
 
+            #Fills working table with imported xml data
             row_count = 0
             while row_count < len(streets):
                 my_db.executeQuery("INSERT INTO addresses_working (first_name, last_name, street, city, state, zip) VALUES ('" + first_names[row_count].getText() + "','" + last_names[row_count].getText() + "','" + streets[row_count].getText() + "','" + cities[row_count].getText() + "','" + states[row_count].getText() + "','" + zips[row_count].getText() + "')")
                 row_count += 1
 
-            #Swaps the data of the addresses and addresses_working tables
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses TO database_automation_assignment.addresses_old")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_working TO database_automation_assignment.addresses")
-            my_db.executeQuery("COMMIT")
-
-            my_db.executeQuery("START TRANSACTION")
-            my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_old TO database_automation_assignment.addresses_working")
-            my_db.executeQuery("COMMIT")
+    #Displays an error if the file is not found
     except:
         print("File not found.")
+
+#Swaps the data of the addresses and addresses_working tables
+my_db.executeQuery("START TRANSACTION")
+my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses TO database_automation_assignment.addresses_old")
+my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_working TO database_automation_assignment.addresses")
+my_db.executeQuery("COMMIT")
+
+my_db.executeQuery("START TRANSACTION")
+my_db.executeQuery("RENAME TABLE database_automation_assignment.addresses_old TO database_automation_assignment.addresses_working")
+my_db.executeQuery("COMMIT")
