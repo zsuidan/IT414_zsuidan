19fef3cf2244be79c18e24e385e26d7b958544fb
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2022-05-30 18:52:27 -0400

    Added instructions and made minor text adjustments

--- system_monitoring_assignment/system_monitoring.py
+++ system_monitoring_assignment/system_monitoring.py
@@ -8,10 +8,10 @@ from twilio.rest import Client
 #Obtains current date and time and formats it
 raw_date = datetime.datetime.fromtimestamp(time.time())
 date = raw_date.strftime('%B %d, %Y %H:%M:%S')
+#Obtains the current hour and minutes
 hour = raw_date.strftime('%H:%M')
-#hour = "6:00"
 
-#Connects to mail server and sends messages
+#Attempts to open the script_config.json file
 try:
     with open("text_files/script_config.json") as config_json:
         #Loads data from script_config.json
@@ -33,7 +33,7 @@ try:
                 core_count += 1
 
             #Creates a string displaying RAM usage
-            ram_string = "\n\nRAM usage:\n\tUsed: " + str(psutil.virtual_memory().used) + "\n\tAvailable: " + str(psutil.virtual_memory().available) + "\n\tPercent used: " + str(psutil.virtual_memory().percent)
+            ram_string = "\n\nRAM usage:\n\tUsed: " + str(psutil.virtual_memory().used) + "\n\tAvailable: " + str(psutil.virtual_memory().available) + "\n\tPercent used: " + str(psutil.virtual_memory().percent) + "%"
             
             #Creates a string displaying disk usage
             volume_string = "\n\nHard disk usage:\n"
@@ -50,71 +50,75 @@ try:
             network_string = "\nNetwork I/O:\n\tBytes sent: " + str(psutil.net_io_counters().bytes_sent) + "\n\tBytes received: " + str(psutil.net_io_counters().bytes_recv) + "\n\tIn errors: " + str(psutil.net_io_counters().errin) + "\n\tOut errors: " + str(psutil.net_io_counters().errout) + "\n\tIn packets dropped: " + str(psutil.net_io_counters().dropin) + "\n\tOut packets dropped: " + str(psutil.net_io_counters().dropout)
 
             #Sends an email with CPU, RAM, disk, and network data
-            my_message = "From: " + config_data['from_email'] + "\nSubject: Test\n" + date + cpu_string + ram_string + volume_string + network_string
+            my_message = "From: " + config_data['from_email'] + "\nSubject: System Resource Status Report\n" + date + cpu_string + ram_string + volume_string + network_string
 
             smtpConn.sendmail(config_data['from_email'], config_data['to_email'], my_message)
-            
-            print(my_message)
 
             #Closes mail server connection
             smtpConn.close()
 
-        #Connects to Twilio SMS client using json data
-        accountID = config_data['twilio_account_id']
-        authToken = config_data['twilio_auth_token']
-        trialNumber = config_data['twilio_trial_number'] # number must be prefixed with a +1, e.g. +12485551212
-        cellNumber = config_data['twilio_cell_number'] # use same number format as above
-
-        twClient = Client(accountID, authToken)
-
-        #If resource usage exceeds a certain threshold, sends a text message alert
-        send_alert = False
-        alert_message = date + "\n"
-
-        #Checks CPU usage and adds alert information if greater than 85%
-        cpu_usage = psutil.cpu_percent(interval=.1)
-        if cpu_usage > 85:
-            send_alert = True
-            alert_message += "\nCPU usage has reached " + str(cpu_usage) + "%\n"
-        
-        #Checks RAM usage and adds alert information if greater than 90%
-        mem_usage = psutil.virtual_memory().percent
-        if mem_usage > 90:
-            send_alert = True
-            alert_message += "\nRAM usage has reached " + str(mem_usage) + "%\n"
-
-        #Checks percent of space used in each volume and adds alert information of a volume exceeds 70%
-        volumes = psutil.disk_partitions()
-        for volume in volumes:
-            space_used = psutil.disk_usage(volume[1]).percent
-            if space_used > 70:
-                send_alert = True
-                alert_message += "\n" + volume[1] + " is " + str(space_used) + "% full and close to running out of space\n"
-
-        #Checks network I/O and adds alert information if errors or dropped packets are appearing
-        net_errors_in = psutil.net_io_counters().errin 
-        net_errors_out = psutil.net_io_counters().errout 
-        net_drop_in = psutil.net_io_counters().dropin 
-        net_drop_out = psutil.net_io_counters().dropout 
-
-        if net_errors_in > 0 or net_errors_out > 0 or net_drop_in > 0 or net_drop_out > 0:
-            send_alert = True
-            alert_message += "\nNetwork issues detected:\n\tIn errors: " + str(net_errors_in) + "\n\tOut errors: " + str(net_errors_out) + "\n\tIn packets dropped: " + str(net_drop_in) + "\n\tOut packets dropped: " + str(net_drop_out)
-
-        #print(alert_message)
+        #Obtains the seconds since the last text alert sent
         current_time = time.time()
         last_text_time = config_data['last_text']
         seconds_since_last_text = current_time - float(last_text_time)
 
-        #If an issue is detected and an alert has not been sent in the last hour, sends the alert message as a text
-        if send_alert and seconds_since_last_text >= 3600:
-            #Sends text message with alert information
-            my_message = twClient.messages.create(body=alert_message, from_=trialNumber, to=cellNumber)
+        #If over an hour (3600 seconds) has passed since the last text alert, checks system again
+        if seconds_since_last_text >= 3600:
 
-            #Updates last_text json value to the current time
-            config_data['last_text'] = str(current_time)
-            with open("text_files/script_config.json", "w") as config_json:
-                json.dump(config_data, config_json)
+            #Connects to Twilio SMS client using json data
+            accountID = config_data['twilio_account_id']
+            authToken = config_data['twilio_auth_token']
+            trialNumber = config_data['twilio_trial_number'] # number must be prefixed with a +1, e.g. +12485551212
+            cellNumber = config_data['twilio_cell_number'] # use same number format as above
+
+            twClient = Client(accountID, authToken)
+
+            #If resource usage exceeds a certain threshold, adds alert information to alert_message and sets send_alert to True
+            send_alert = False
+            alert_message = date + "\n"
+
+            #Checks CPU usage and adds alert information if greater than 85%
+            cpu_usage = psutil.cpu_percent(interval=.1)
+            if cpu_usage > 85:
+                send_alert = True
+                alert_message += "\nCPU usage has reached " + str(cpu_usage) + "%\n"
+            
+            #Checks RAM usage and adds alert information if greater than 90%
+            mem_usage = psutil.virtual_memory().percent
+            if mem_usage > 90:
+                send_alert = True
+                alert_message += "\nRAM usage has reached " + str(mem_usage) + "%\n"
+
+            #Checks percent of space used in each volume and adds alert information of a volume exceeds 70%
+            volumes = psutil.disk_partitions()
+            for volume in volumes:
+                space_used = psutil.disk_usage(volume[1]).percent
+                if space_used > 70:
+                    send_alert = True
+                    alert_message += "\n" + volume[1] + " is " + str(space_used) + "% full and close to running out of space\n"
+
+            #Checks network I/O and adds alert information if errors or dropped packets are appearing
+            net_errors_in = psutil.net_io_counters().errin 
+            net_errors_out = psutil.net_io_counters().errout 
+            net_drop_in = psutil.net_io_counters().dropin 
+            net_drop_out = psutil.net_io_counters().dropout 
+
+            if net_errors_in > 0 or net_errors_out > 0 or net_drop_in > 0 or net_drop_out > 0:
+                send_alert = True
+                alert_message += "\nNetwork issues detected:\n\tIn errors: " + str(net_errors_in) + "\n\tOut errors: " + str(net_errors_out) + "\n\tIn packets dropped: " + str(net_drop_in) + "\n\tOut packets dropped: " + str(net_drop_out)
+
+            #If an issue is detected, sends the alert message as a text
+            if send_alert:
+                #Sends text message with alert information
+                alert_message += "\n\nPlease fix these issues immediately."
+                my_message = twClient.messages.create(body=alert_message, from_=trialNumber, to=cellNumber)
+
+                #Updates last_text json value to the current time
+                config_data['last_text'] = str(current_time)
+                with open("text_files/script_config.json", "w") as config_json:
+                    json.dump(config_data, config_json)
+
+#If the script_config.json file is not found, prints an error and exits
 except:
     print("Config file not found.")
     exit()
